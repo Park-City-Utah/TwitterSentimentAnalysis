01a2c156ceaa8021dcfe9ea7e9f565929c4fdca1
Parker Ferguson <parkcity.pf@gmail.com>
2021-01-06 12:02:27 -0800

    Initial commit of Twitter Sentiment Analysis application in Python. Tweepy required for Twitter API tweet retrieval, textBlob required for Sentiment Analysis on Tweets. Pandas DataFrame and matplotlib required for Visualization (histogram).

---
+++ TwitterSentimentAnalysis.py
@@ -0,0 +1,39 @@
+import tweepy
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from textblob import TextBlob
+
+#Authentical with tweepy OAuth and Twitter API
+apiKey= 'CONSUMER_KEY_HERE'
+apiSecret= 'CONSUMER_SECRET_HERE'
+accessToken='ACCESS_TOKEN_HERE'
+accessTokenSecret='ACCESS_TOKEN_SECRET_HERE'
+
+auth = tweepy.OAuthHandler(apiKey, apiSecret)
+auth.set_access_token(accessToken, accessTokenSecret)
+
+api = tweepy.API(auth)
+
+#Pull Tweets on keyword
+tweets = api.search('#datadriven',count=1000)
+
+polarity = []
+subjectivity = []
+
+#Perform analysis and create lists for manipulation (polarity/subjectivity)
+for tweet in tweets:
+    analysis = TextBlob(tweet.text)
+
+    #Create list of polarity & subjectivity for mean
+    polarity.append(analysis.sentiment.polarity)
+    subjectivity.append(analysis.sentiment.subjectivity)  
+
+#Generate histogram
+#sentimentDF = pd.DataFrame({'Polarity': polarity, 'Subjectivity': subjectivity})
+polarityDf = pd.DataFrame(polarity, columns=["Polarity"])
+subjectivityDf = pd.DataFrame(subjectivity, columns=["Subjectivity"])
+
+polarityDf.hist(color="blue")
+subjectivityDf.hist(color= "orange")
+plt.show()      
